2aad2f1b3072f8d379da462c09d838eaad16f283
Navnath Jadhav <101859748+navnath99999@users.noreply.github.com>
2023-03-25 14:08:42 +0530

    Add files via upload

---
+++ Snake_Game/src/game/SnakeGame.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package game;
+
+/**
+ *
+ * @author babur
+ */
+
+//***************************************
+public class SnakeGame {
+
+	public static void main(String[] args) {
+		
+		new GameFrame();
+	}
+}
+//***************************************
+
+
+
+//***************************************
+
+	
+	
+//***************************************
